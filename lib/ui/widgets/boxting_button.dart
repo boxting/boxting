39e1c81d8ad22d8a8e9617df1862d40622525333
Enzo Lizama <lizama.enzo@gmail.com>
2020-10-19 02:39:29 -0500

    add some basic widgets

---
+++ lib/ui/widgets/boxting_button.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class BoxtingButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/ui/widgets/error_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class BoxtingErrorScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
