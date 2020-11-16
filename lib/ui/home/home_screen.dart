ba9092a16e77df5b7684d8e94c923c458153de2d
Enzo Lizama <lizama.enzo@gmail.com>
2020-11-15 19:32:28 -0500

    home screen with hooks

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -2,9 +2,10 @@ import 'package:boxting/ui/home/home_bloc.dart';
 import 'package:boxting/ui/settings/settings_screen.dart';
 import 'package:ff_navigation_bar/ff_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:provider/provider.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends HookWidget {
   HomeScreen._();
 
   static Widget init(BuildContext context) {
@@ -15,33 +16,28 @@ class HomeScreen extends StatefulWidget {
   }
 
   @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int selectedIndex = 0;
-  Widget child;
-  Widget getChildByIndex(int index) {
-    switch (index) {
-      case 0:
-        child = Placeholder();
-        break;
-      case 1:
-        child = Placeholder();
-        break;
-      case 2:
-        child = SettingsScreen.init(context);
-        break;
-      default:
-    }
-    return child;
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final selectedIndex = useState<int>(0);
+    final child = useState<Widget>();
+    Widget getChildByIndex(int index, BuildContext context) {
+      switch (index) {
+        case 0:
+          child.value = Placeholder();
+          break;
+        case 1:
+          child.value = Placeholder();
+          break;
+        case 2:
+          child.value = SettingsScreen.init(context);
+          break;
+        default:
+      }
+      return child.value;
+    }
+
     return Scaffold(
       appBar: AppBar(),
-      body: child,
+      body: child.value,
       bottomNavigationBar: FFNavigationBar(
         theme: FFNavigationBarTheme(
           barBackgroundColor: Colors.white,
@@ -49,12 +45,10 @@ class _HomeScreenState extends State<HomeScreen> {
           selectedItemIconColor: Colors.white,
           selectedItemLabelColor: Colors.black,
         ),
-        selectedIndex: selectedIndex,
+        selectedIndex: selectedIndex.value,
         onSelectTab: (index) {
-          setState(() {
-            selectedIndex = index;
-            getChildByIndex(selectedIndex);
-          });
+          selectedIndex.value = index;
+          getChildByIndex(selectedIndex.value, context);
         },
         items: [
           FFNavigationBarItem(
