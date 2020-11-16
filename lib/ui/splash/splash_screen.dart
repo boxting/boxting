490a53f72ed33ba850a22fda21dc00ccc9add9a7
Enzo Lizama <lizama.enzo@gmail.com>
2020-11-15 19:32:10 -0500

    splashs creen

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,8 +1,11 @@
+import 'package:boxting/ui/login/login_screen.dart';
+import 'package:boxting/ui/onboarding/onboarding_screen.dart';
 import 'package:boxting/ui/splash/splash_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:provider/provider.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends HookWidget {
   SplashScreen._();
 
   static Widget init(BuildContext context) {
@@ -14,6 +17,46 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final provider = context.watch<SplashBloc>();
+    useEffect(() {
+      Future.microtask(() async {
+        try {
+          await Future.delayed(Duration(seconds: 1));
+          await provider.isFirstTimeOpen();
+          if (provider.isFirstTime) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (_) => OnBoardingScreen(),
+              ),
+            );
+          } else {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (_) => LoginScreen.init(context),
+              ),
+            );
+          }
+        } catch (_) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (_) => OnBoardingScreen(),
+            ),
+          );
+        }
+      });
+      return;
+    });
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          'assets/images/boxting_logo.png',
+          height: 256,
+          width: 256,
+        ),
+      ),
+    );
   }
 }
