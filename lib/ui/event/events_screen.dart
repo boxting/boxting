8b48bc3455f2677b678d488cdd6845fef1d6dfa7
Enzo Lizama <lizama.enzo@gmail.com>
2020-12-30 22:11:37 -0500

    update cocoa pods

---
+++ lib/ui/event/events_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class EventsScreen extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: Column(),
+    );
+  }
+}
