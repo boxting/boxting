52772a696d61857547459710fd58fa6e3c3b8e71
Enzo Lizama <lizama.enzo@gmail.com>
2021-01-10 19:34:40 -0500

    move local auth logic to biometric bloc

--- lib/features/biometric/biometric_bloc.dart
+++ lib/features/biometric/biometric_bloc.dart
@@ -1,14 +1,86 @@
 import 'package:boxting/domain/repository/settings_repository.dart';
+import 'package:boxting/features/home/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
 
 enum BiometricState { initial, loading }
 
+typedef void FailureCallback(PlatformException e);
+
 class BiometricBloc extends ChangeNotifier {
   final SettingsRepository repository;
+  final LocalAuthentication auth;
 
-  BiometricBloc({@required this.repository});
+  BiometricBloc(this.repository, this.auth);
 
-  Future<void> setBiometricInformation(bool enabled) async {
+  Future<void> checkBiometrics() async {
+    try {
+      await auth.canCheckBiometrics;
+    } on PlatformException catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> authenticate({
+    BuildContext context,
+    VoidCallback onSuccess,
+    FailureCallback onFailure,
+  }) async {
+    bool authenticated = false;
+    try {
+      authenticated = await auth.authenticateWithBiometrics(
+        localizedReason: 'Scan your fingerprint to authenticate',
+        useErrorDialogs: true,
+        stickyAuth: true,
+      );
+      if (authenticated) {
+        _setBiometricInformation(authenticated);
+        onSuccess();
+      } else {
+        throw PlatformException(code: 'La autenticación fallo');
+      }
+    } on PlatformException catch (e) {
+      onFailure(e);
+    }
+  }
+
+  void goToHomeScreen(
+    BuildContext context, {
+    bool dialog = false,
+    bool comesFromSettings = false,
+  }) {
+    if (comesFromSettings) {
+      // Pop the dialog
+      if (dialog) Navigator.pop(context);
+      // Pop the Biometric Screen
+      Navigator.pop(context);
+    } else {
+      // Pop the dialog
+      if (dialog) Navigator.pop(context);
+      // Go to HomeScreen
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (_) => HomeScreen.init(context),
+        ),
+      );
+    }
+  }
+
+  Future<void> getAvailableBiometrics() async {
+    try {
+      await auth.getAvailableBiometrics();
+    } on PlatformException catch (e) {
+      print(e);
+    }
+  }
+
+  void cancelAuthentication() {
+    auth.stopAuthentication();
+  }
+
+  Future<void> _setBiometricInformation(bool enabled) async {
     await repository.setFingerprintLogin(enabled);
   }
 }
--- lib/features/biometric/biometric_screen.dart
+++ lib/features/biometric/biometric_screen.dart
@@ -1,8 +1,6 @@
 import 'package:boxting/data/repository/settings_repository_impl.dart';
 import 'package:boxting/features/biometric/biometric_bloc.dart';
-import 'package:boxting/features/home/home_screen.dart';
 import 'package:boxting/widgets/widgets.dart';
-
 import 'package:cool_alert/cool_alert.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -12,13 +10,12 @@ import 'package:provider/provider.dart';
 
 class BiometricScreen extends HookWidget {
   BiometricScreen._({this.comesFromSetting});
-
   final bool comesFromSetting;
-  final LocalAuthentication auth = LocalAuthentication();
 
   static Widget init(BuildContext context, {bool settings}) {
     return ChangeNotifierProvider(
-      create: (_) => BiometricBloc(repository: SettingsRepositoryImpl()),
+      create: (_) =>
+          BiometricBloc(SettingsRepositoryImpl(), LocalAuthentication()),
       builder: (_, __) => BiometricScreen._(
         comesFromSetting: settings ?? false,
       ),
@@ -28,80 +25,7 @@ class BiometricScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final _isAuthenticating = useState<bool>(false);
-
-    Future<void> _checkBiometrics() async {
-      try {
-        await auth.canCheckBiometrics;
-      } on PlatformException catch (e) {
-        print(e);
-      }
-    }
-
-    void goToHomeScreen(BuildContext context, {bool dialog = false}) {
-      if (comesFromSetting) {
-        // Pop the dialog
-        if (dialog) Navigator.pop(context);
-        // Pop the Biometric Screen
-        Navigator.pop(context);
-      } else {
-        // Pop the dialog
-        if (dialog) Navigator.pop(context);
-        // Go to HomeScreen
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (_) => HomeScreen.init(context),
-          ),
-        );
-      }
-    }
-
-    Future<void> _getAvailableBiometrics() async {
-      try {
-        await auth.getAvailableBiometrics();
-      } on PlatformException catch (e) {
-        print(e);
-      }
-    }
-
-    Future<void> _authenticate(BuildContext context) async {
-      final bloc = context.read<BiometricBloc>();
-      bool authenticated = false;
-      try {
-        authenticated = await auth.authenticateWithBiometrics(
-          localizedReason: 'Scan your fingerprint to authenticate',
-          useErrorDialogs: true,
-          stickyAuth: true,
-        );
-        if (authenticated) {
-          bloc.setBiometricInformation(authenticated);
-          CoolAlert.show(
-            context: context,
-            type: CoolAlertType.success,
-            title: "Perfecto",
-            text: "Tu huella digital ha sido validada",
-            confirmBtnText: 'Continuar',
-            onConfirmBtnTap: () => goToHomeScreen(context, dialog: true),
-          );
-        } else {
-          throw PlatformException(code: 'La autenticación fallo');
-        }
-      } on PlatformException catch (e) {
-        CoolAlert.show(
-            context: context,
-            type: CoolAlertType.error,
-            title: "Algo salio mal",
-            text: e.message,
-            onConfirmBtnTap: () {
-              Navigator.pop(context);
-              _isAuthenticating.value = false;
-            });
-      }
-    }
-
-    void _cancelAuthentication() {
-      auth.stopAuthentication();
-    }
+    final bloc = context.watch<BiometricBloc>();
 
     final size = MediaQuery.of(context).size;
     return Scaffold(
@@ -138,17 +62,40 @@ class BiometricScreen extends HookWidget {
                   ),
                 ),
                 onPressed: () async {
-                  await _checkBiometrics();
-                  await _getAvailableBiometrics();
+                  await bloc.checkBiometrics();
+                  await bloc.getAvailableBiometrics();
                   if (_isAuthenticating.value)
-                    _cancelAuthentication();
+                    bloc.cancelAuthentication();
                   else
-                    _authenticate(context);
+                    bloc.authenticate(
+                      context: context,
+                      onSuccess: () => CoolAlert.show(
+                        context: context,
+                        type: CoolAlertType.success,
+                        title: "Perfecto",
+                        text: "Tu huella digital ha sido validada",
+                        confirmBtnText: 'Continuar',
+                        onConfirmBtnTap: () => bloc.goToHomeScreen(
+                          context,
+                          dialog: true,
+                          comesFromSettings: comesFromSetting,
+                        ),
+                      ),
+                      onFailure: (PlatformException e) => CoolAlert.show(
+                          context: context,
+                          type: CoolAlertType.error,
+                          title: "Algo salio mal",
+                          text: e.message,
+                          onConfirmBtnTap: () {
+                            Navigator.pop(context);
+                            _isAuthenticating.value = false;
+                          }),
+                    );
                 },
               ),
               SizedBox(height: 24),
               InkWell(
-                onTap: () => goToHomeScreen(context),
+                onTap: () => bloc.goToHomeScreen(context),
                 child: Text('Omitir por ahora'),
               )
             ],
