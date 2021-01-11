458afd9316119ab6affb63484a34a4c9d5514e6b
Enzo Lizama <lizama.enzo@gmail.com>
2021-01-10 19:34:25 -0500

    move to fragment activity

--- android/app/src/main/kotlin/com/boxtinglabs/boxting/MainActivity.kt
+++ android/app/src/main/kotlin/com/boxtinglabs/boxting/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.boxtinglabs.boxting
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import androidx.annotation.NonNull
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
