f8eb13382e852225c7dd3ed2a53f6ebec753ebb9
Enzo Lizama <lizama.enzo@gmail.com>
2021-01-16 20:32:25 -0500

    add testing

---
+++ test/utils/mocks.dart
@@ -0,0 +1,7 @@
+import 'package:boxting/data/network/request/login_request/login_request.dart';
+import 'package:boxting/data/network/request/register_request/register_request.dart';
+import 'package:mockito/mockito.dart';
+
+class MockLoginRequest extends Mock implements LoginRequest {}
+
+class MockRegisterRequest extends Mock implements RegisterRequest {}
