ba019da3e4906ef788b74fe57eeaf78e4974cd85
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2021-01-23 21:34:44 -0500

    V0.0.2 (#15)
    * update run for workflow
    
    * see gpg version
    
    * update format
    
    * update flutter release
    
    * remove warning options
    
    * convert to apk
    
    * update cd fastlane version
    
    * update lane name
    
    * update ci cd
    
    * update ruby version
    
    * update ruby version and lockfile
    
    * update
    
    * update gemfile lock
    
    * cicd
    
    * update gem
    
    * updte gem
    
    * update ruby version
    
    * update fastfile
    
    * udpate fastfile
    
    * add project dir
    
    * typo fix
    
    * update project dir
    
    * update cicd
    
    * ci/cd fix
    
    * find gradlew
    
    * echo
    
    * subdirectory android
    
    * fastfile
    
    * update flutter version
    
    * fastfile
    
    * another try
    
    * gradle update
    
    * sh fastlane
    
    * try sh
    
    * sh command fix
    
    * proj dir
    
    * try gradlew out
    
    * ruby version
    
    * android
    
    * functionally

--- .github/scripts/decrypt_android_keys.sh
+++ .github/scripts/decrypt_android_keys.sh
@@ -2,5 +2,6 @@
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
+gpg --version
 gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_KEYS_SECRET_PASSPHRASE" \
 --output android/android_keys.zip android/android_keys.zip.gpg && cd android && jar xvf android_keys.zip && mv google-services.json app/ && cd -
