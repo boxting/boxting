611c67baa69a32d21894a52323cf6ee79e83be6c
Enzo Lizama <lizama.enzo@gmail.com>
2021-01-31 22:11:52 -0500

    update primary color

--- lib/boxting_theme.dart
+++ lib/boxting_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 final boxtingTheme = ThemeData(
-  primaryColor: Color(0xff6200EE),
+  primaryColor: Color(0xff004AAD),
   accentColor: Color(0xff79F6C0),
   shadowColor: Color(0xff04192F),
   canvasColor: Color(0xffFFFFFF),
