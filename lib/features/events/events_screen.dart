9f8bef412b71ba9edce9abccbecf40ddfec76e9a
Enzo Lizama <lizama.enzo@gmail.com>
2021-02-16 00:39:36 -0500

    empty state for event list

--- lib/features/events/events_screen.dart
+++ lib/features/events/events_screen.dart
@@ -1,3 +1,4 @@
+import 'package:boxting/data/network/response/event_response/event_response.dart';
 import 'package:boxting/domain/repository/event_repository.dart';
 import 'package:boxting/features/events/event_item.dart';
 import 'package:boxting/features/events/events_bloc.dart';
@@ -6,6 +7,7 @@ import 'package:boxting/service_locator.dart';
 import 'package:boxting/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 
 class EventsScreen extends HookWidget {
@@ -21,33 +23,62 @@ class EventsScreen extends HookWidget {
     final bloc = context.watch<EventsBloc>();
     return BoxtingScaffold(
       appBar: BoxtingAppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          children: [
-            Text('Las siguientes votaciones son:'),
-            Expanded(
-              child: FutureBuilder(
-                future: bloc.fetchEvents(),
-                builder: (_, __) {
-                  if (bloc.events != null) {
-                    return ListView.builder(
-                      itemCount: bloc.events.length,
-                      itemBuilder: (_, int index) =>
-                          EventItem(event: bloc.events[index]),
-                    );
-                  }
-                  return Center(child: CircularProgressIndicator());
-                },
-              ),
-            )
-          ],
-        ),
+      body: FutureBuilder(
+        future: bloc.fetchEvents(),
+        builder: (_, __) {
+          if (bloc.events != null) {
+            return Center(child: EventListBody(events: bloc.events));
+          }
+          return Center(child: CircularProgressIndicator());
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => SubscribeEventScreen.navigate(context),
         child: Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class EventListBody extends StatelessWidget {
+  final List<EventResponseData> events;
+  const EventListBody({Key key, this.events}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    if (events.isNotEmpty) {
+      return EmptyEventList();
+    } else {
+      return Column(
+        children: [
+          Text('Las siguientes votaciones son:'),
+          Expanded(
+            child: ListView.builder(
+              itemCount: events.length,
+              itemBuilder: (_, int index) => EventItem(event: events[index]),
+            ),
+          ),
+        ],
+      );
+    }
+  }
+}
+
+class EmptyEventList extends StatelessWidget {
+  const EmptyEventList({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Lottie.asset(
+          'assets/lottie/sad_empty_box.json',
+          width: 240,
+          height: 240,
+          fit: BoxFit.fill,
+        ),
+        Text('Aún no estas registrado en ningún evento de votación')
+      ],
     );
   }
 }
