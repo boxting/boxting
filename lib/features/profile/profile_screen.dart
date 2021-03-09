409f05f3aa661a164934166e5569ceea8c82d867
Enzo Lizama <lizama.enzo@gmail.com>
2021-03-08 21:21:53 -0500

    profile screen ui

--- lib/features/profile/profile_screen.dart
+++ lib/features/profile/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:boxting/domain/entities/user.dart';
 import 'package:boxting/features/profile/profile_bloc.dart';
 import 'package:boxting/widgets/widgets.dart';
 import 'package:flutter/material.dart';
@@ -8,20 +9,40 @@ import '../../service_locator.dart';
 class ProfileScreen extends StatelessWidget {
   static Widget init(BuildContext context) {
     return ChangeNotifierProvider.value(
-      value: getIt.get<ProfileBloc>(),
+      value: getIt.get<ProfileBloc>()..getUserInformation(),
       builder: (_, __) => ProfileScreen(),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    final bloc = context.watch<ProfileBloc>();
     return BoxtingScaffold(
       appBar: BoxtingAppBar(),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [],
-        ),
+        child: bloc.user != null
+            ? ProfileScreenBody(user: bloc.user)
+            : BoxtingLoadingScreen(),
+      ),
+    );
+  }
+}
+
+class ProfileScreenBody extends StatelessWidget {
+  final User user;
+
+  const ProfileScreenBody({Key key, this.user}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text(user.name),
+          Text(user.phone),
+          Text(user.dni),
+          Text(user.mail)
+        ],
       ),
     );
   }
