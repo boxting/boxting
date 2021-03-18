a3e3685f7d42a524d18aab99780c1de5c36d026b
Enzo Lizama <lizama.enzo@gmail.com>
2021-03-18 17:25:53 -0500

    voting provider

---
+++ lib/features/voting/sucess_voting_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class SuccessVotingScreen extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(),
+    );
+  }
+}
