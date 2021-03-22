1c1a8b61ad8f479aeaefdaf0a073241c894edec6
Enzo Lizama <lizama.enzo@gmail.com>
2021-03-22 01:37:13 -0500

    improve candidate detail

--- lib/features/candidates/detail/candidate_detail_screen.dart
+++ lib/features/candidates/detail/candidate_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:boxting/data/network/response/candidates_response/candidates_response.dart';
 import 'package:boxting/features/candidates/providers.dart';
+import 'package:boxting/widgets/styles.dart';
 import 'package:boxting/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -47,8 +48,25 @@ class CandidateDetailBody extends HookWidget {
       padding: const EdgeInsets.all(20.0),
       child: Column(
         children: [
-          Text('${candidate.lastName}, ${candidate.firstName}'),
+          Image.network(candidate.imageUrl, height: 120, width: 120),
+          SizedBox(height: 48),
+          Text(
+            '${candidate.firstName} ${candidate.lastName} (${candidate.age} años)',
+            style: titleTextStyle,
+          ),
+          SizedBox(height: 24),
           Text(candidate.information),
+          SizedBox(height: 48),
+          Text('Candidato por parte de la lista', style: subTitleTextStyle),
+          SizedBox(height: 48),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.network(candidate.list.imageUrl, height: 86),
+              SizedBox(width: 48),
+              Text(candidate.list.name, style: titleTextStyle)
+            ],
+          )
         ],
       ),
     );
