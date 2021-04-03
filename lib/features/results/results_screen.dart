63bf26ee3039ac63e9c1df6c966aeeb886f9a32d
Enzo Lizama <lizama.enzo@gmail.com>
2021-04-03 16:26:10 -0500

    screen ui

---
+++ lib/features/results/results_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class ResultsScreen extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
